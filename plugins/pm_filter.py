d0fa8e9736d99f48e2110a303abac4a955a3f434
Jnanesh <99884442+Jnanesh124@users.noreply.github.com>
2024-07-06 21:36:53 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -2841,9 +2841,9 @@ async def auto_filter(client, name, msg, reply_msg, ai_search, spoll=False):
                 cap += f"<b>\n📁 <a href='https://telegram.me/{temp.U_NAME}?start=files_{file.file_id}'>[{get_size(file.file_size)}] {' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@') and not x.startswith('www.'), file.file_name.split()))}\n</a></b>"
     else:
         if settings["button"]:
-            cap = f"<b>📪 𝐑𝐞𝐪𝐮𝐢𝐬𝐭𝐞𝐝 𝐌𝐨𝐯𝐢𝐞 ━➣ {search}\n🪬 𝐑𝐞𝐪𝐮𝐢𝐬𝐭𝐞𝐝 𝐏𝐞𝐫𝐬𝐨𝐧 ━➣ {message.from_user.mention}\n📫 𝐑𝐞𝐪𝐮𝐢𝐬𝐭𝐞𝐝 𝐆𝐨𝐮𝐩 ━➣ {message.chat.title}\n🏃 𝐌𝐨𝐯𝐢𝐞 𝐒𝐞𝐚𝐫𝐜𝐡𝐢𝐧𝐠 𝐒𝐩𝐞𝐞𝐝 ━➣ {remaining_seconds} sᴇᴄᴏɴᴅs\n\n</b>"
+            cap = f"<b>📪 𝐌𝐨𝐯𝐢𝐞 ━➣ {search}\n🪬 𝐏𝐞𝐫𝐬𝐨𝐧 ━➣ {message.from_user.mention}\n🏃 𝐒𝐩𝐞𝐞𝐝 ━➣ {remaining_seconds} sᴇᴄᴏɴᴅs\n\n</b>"
         else:
-            cap = f"<b>📪 𝐑𝐞𝐪𝐮𝐢𝐬𝐭𝐞𝐝 𝐌𝐨𝐯𝐢𝐞 ━➣ {search}\n🪬 𝐑𝐞𝐪𝐮𝐢𝐬𝐭𝐞𝐝 𝐏𝐞𝐫𝐬𝐨𝐧 ━➣ {message.from_user.mention}\n📫 𝐑𝐞𝐪𝐮𝐢𝐬𝐭𝐞𝐝 𝐆𝐨𝐮𝐩 ━➣ {message.chat.title}\n🏃 𝐌𝐨𝐯𝐢𝐞 𝐒𝐞𝐚𝐫𝐜𝐡𝐢𝐧𝐠 𝐒𝐩𝐞𝐞𝐝 ━➣ {remaining_seconds} sᴇᴄᴏɴᴅs\n\n</b>"
+            cap = f"<b>📪 𝐌𝐨𝐯𝐢𝐞 ━➣ {search}\n🪬 𝐏𝐞𝐫𝐬𝐨𝐧 ━➣ {message.from_user.mention}\n🏃 𝐒𝐩𝐞𝐞𝐝 ━➣ {remaining_seconds} sᴇᴄᴏɴᴅs\n\n</b>"
             cap+="<b><u>🍿 Your Movie Files 👇</u></b>\n\n"
             for file in files:
                 cap += f"<b>📁 <a href='https://telegram.me/{temp.U_NAME}?start=files_{file.file_id}'>[{get_size(file.file_size)}] {' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@') and not x.startswith('www.'), file.file_name.split()))}\n\n</a></b>"
